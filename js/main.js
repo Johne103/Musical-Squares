26581feabed9f3e794c4792646e5cc2e2de37dc4
John Evans <johne103@hotmail.com>
2016-10-10 23:54:58 +0100

    further js updates for butonss

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-function shuffle(a) {
-  var j, x, i;
-  for (i = a.length; i; i--) {
-    j = Math.floor(Math.random() * i);
-    x = a[i - 1];
-    a[i - 1] = a[j];
-    a[j] = x;
-  }
-}
+
+    function shuffle(a) {
+      var j, x, i;
+      for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+      }
+    }
 
 $(function() {
   var $cpuSquares = $('#cpuSquares div');
@@ -17,6 +18,45 @@ $(function() {
   var currentIndex = 0;
   var userScore = 0;
   var feedBack = "";
+  var responseTime = 500;
+  var easyBtn = document.getElementById("easy");
+  var medBtn = document.getElementById("medium");
+  var hardBtn = document.getElementById("hard");
+  var resetBtn = document.getElementById("reset");
+
+
+  easyBtn.addEventListener("click", setEasy);
+  medBtn.addEventListener("click", setMed);
+  hardBtn.addEventListener("click", setHard);
+  resetBtn.addEventListener("click", resetGame);
+
+  function timer (b) {
+    return b;
+  }
+
+  function setEasy () {
+    var responseTime = timer(5000);
+    console.log("responseTime " + responseTime);
+    playGame();
+   }
+
+  function setMed () {
+     var responseTime = timer(3000);
+     console.log("responseTime " + responseTime);
+     playGame();
+    }
+
+  function setHard () {
+      var responseTime = timer(1000);
+      console.log("responseTime " + responseTime);
+      playGame();
+     }
+
+  function resetGame () {
+    playGame ();
+  }
+
+  function playGame () {
 
   $score.text(userScore);
 
@@ -29,8 +69,8 @@ $(function() {
       audio.src = "sounds/" + $(square).data('filename') + ".wav";
       console.log(audio.src);
       audio.play();
-      $(square).fadeTo(2000, 0);
-    }, i * 2000);
+      $(square).fadeTo(5000, 0);
+    }, i * 5000);
   });
 
   $userSquares.one('click', function() {
@@ -48,4 +88,5 @@ $(function() {
       $(this).css({ opacity: 0 });
     }
   });
+}
 });
