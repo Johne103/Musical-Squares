b907702a5b4149f9404b12bf5314a23291ada192
John Evans <johne103@hotmail.com>
2016-10-10 16:13:13 +0100

    css updated to provide user images

--- js/main.js
+++ js/main.js
@@ -1,42 +1,51 @@
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length; i; i--) {
+    j = Math.floor(Math.random() * i);
+    x = a[i - 1];
+    a[i - 1] = a[j];
+    a[j] = x;
+  }
+}
+
 $(function() {
   var $cpuSquares = $('#cpuSquares div');
   var $userSquares = $('#userSquares div');
   var audio = $('#soundToPlay')[0];
+  var $score = $('#score');
+  var $feedback = $('#feedback');
   var currentIndex = 0;
-  var userScore = [];
+  var userScore = 0;
+  var feedBack = "";
+
+  $score.text(userScore);
+
+  shuffle($cpuSquares);
 
   $cpuSquares.each(function(i, square) {
     var parent = $("#cpuSquares");
-    var divs = parent.children();
-    while (divs.length) {
-        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-    }
     setTimeout(function() {
       currentIndex = i;
       audio.src = "sounds/" + $(square).data('filename') + ".wav";
       console.log(audio.src);
       audio.play();
-      $(square).fadeTo(1000, 0);
-    }, i * 1000);
+      $(square).fadeTo(2000, 0);
+    }, i * 2000);
   });
 
   $userSquares.one('click', function() {
     var parent = $("#userSquares");
-    var divs = parent.children();
-    while (divs.length) {
-        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-    }
     var filename = $(this).data('filename') + ".wav";
     var fileBeingPlayed = audio.src.split('/').slice(-1)[0];
-    console.log(filename, fileBeingPlayed);
     if(filename === fileBeingPlayed) {
       $cpuSquares.eq(currentIndex).stop(true, false);
       userScore++;
       console.log(userScore);
+      $score.text(userScore);
+      $(this).css({background: "red"});
+    }
+    else {
+      $(this).css({ opacity: 0 });
     }
   });
-
-document.getElementById("Score").textContent = userScore;
-document.getElementById("Feedback").textContent = "Testing";
-
 });
