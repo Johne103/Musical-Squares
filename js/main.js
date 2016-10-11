2ff8059bc43cc75c72bb533fcdd1195cfdfdfd7b
John Evans <johne103@hotmail.com>
2016-10-11 15:15:55 +0100

    Scale to 4X4 grid

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 
-    function shuffle(a) {
-      var j, x, i;
-      for (i = a.length; i; i--) {
-        j = Math.floor(Math.random() * i);
-        x = a[i - 1];
-        a[i - 1] = a[j];
-        a[j] = x;
-      }
-    }
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length; i; i--) {
+    j = Math.floor(Math.random() * i);
+    x = a[i - 1];
+    a[i - 1] = a[j];
+    a[j] = x;
+  }
+}
 
 $(function() {
   var $cpuSquares = $('#cpuSquares div');
@@ -16,115 +16,160 @@ $(function() {
   var $score1 = $('#score1');
   var $score2 = $('#score2');
   var $result = $('#result');
+  var $playerTurn = $('#playerTurn');
   var currentIndex = 0;
   var user1Score = 0;
   var user2Score = 0;
   var clickCounter = 0;
-  var feedBack = "";
-  var responseTime = 500;
-  var easyBtn = document.getElementById("easy");
-  // var medBtn = document.getElementById("medium");
-  var hardBtn = document.getElementById("hard");
-  var resetBtn = document.getElementById("reset");
+  var fadeCounter = 0;
+  var whosTurn = "";
+  var $easyBtn = $("#easy");
+  var $medBtn = $("#medium");
+  var $hardBtn = $("#hard");
+  var $resetBtn = $("#reset");
+  var responseTime = 5000;
+  var timers;
 
 
-  easyBtn.addEventListener("click", setEasy);
-  //medBtn.addEventListener("click", setMed);
-  hardBtn.addEventListener("click", setHard);
-  resetBtn.addEventListener("click", resetGame);
-
-  function timer (b) {
-    return b;
-  }
+  $easyBtn.on("click", setEasy);
+  $medBtn.on("click", setMed);
+  $hardBtn.on("click", setHard);
+  $resetBtn.on("click", resetGame);
 
   function setEasy () {
-    var responseTime = timer(5000);
+    responseTime = 5000;
     console.log("responseTime " + responseTime);
+    $(this).addClass("active");
     playGame();
-   }
+  }
 
-  // function setMed () {
-  //    var responseTime = timer(3000);
-  //    console.log("responseTime " + responseTime);
-  //    playGame();
-  //   }
+  function setMed () {
+     var responseTime = 3000;
+     console.log("responseTime " + responseTime);
+     $(this).addClass("active");
+     playGame();
+    }
 
   function setHard () {
-      var responseTime = timer(1000);
-      console.log("responseTime " + responseTime);
-      playGame();
-     }
+    responseTime = 1000;
+    console.log("responseTime " + responseTime);
+    $(this).addClass("active");
+    playGame();
+  }
 
   function resetGame () {
     console.log('reset pressed');
-    playGame ();
+
+    clickCounter = 0;
+    fadeCounter = 0;
+
+    user1Score = 0;
+    user2Score = 0;
+
+    $score1.text(user1Score);
+    $score2.text(user2Score);
+
+    whosTurn = "";
+
+    $("button").removeClass('active');
+
+    $playerTurn.text(whosTurn);
+
+    timers.forEach(function(timerId) {
+      clearTimeout(timerId);
+    });
+
+    $cpuSquares
+      .stop(true, false)
+      .css({ opacity: 1 });
+
+    $userSquares
+      .removeAttr("style")
+      .css({ opacity: 1});
+
+    $result.text("");
   }
 
-  function playGame () {
+  function playGame() {
 
-  $score1.text(user1Score);
-  $score2.text(user1Score);
+    $score1.text(user1Score);
+    $score2.text(user1Score);
 
+    $playerTurn.text(whosTurn);
 
-  shuffle($cpuSquares);
+    shuffle($cpuSquares);
 
-  $cpuSquares.each(function(i, square) {
-    var parent = $("#cpuSquares");
-    setTimeout(function() {
-      currentIndex = i;
-      audio.src = "sounds/" + $(square).data('filename') + ".wav";
-      console.log(audio.src);
-      audio.play();
-      $(square).fadeTo(responseTime, 0);
-    }, i * responseTime);
-  });
+    timers = $cpuSquares.toArray().map(function(square, i) {
+      var parent = $("#cpuSquares");
+      return setTimeout(function() {
+        currentIndex = i;
+        audio.src = "sounds/" + $(square).data('filename') + ".wav";
+        console.log(audio.src);
+        audio.play();
+        $(square).fadeTo(responseTime, 0);
+        setTimeout(function() {
+          fadeCounter++;
+          console.log('fadecounter ' + fadeCounter + $cpuSquares.length);
+          if (fadeCounter == $cpuSquares.length) {
 
-  $userSquares.one('click', function() {
+            getWinner(user1Score, user2Score);
+          }
+        }, responseTime);
+      }, i * responseTime);
+    });
 
+    $userSquares.one('click', function() {
 
       clickCounter++;
+      $playerTurn.toggleClass("green");
       console.log('clickCounter ' + clickCounter);
-    var parent = $("#userSquares");
-    var filename = $(this).data('filename') + ".wav";
-    var fileBeingPlayed = audio.src.split('/').slice(-1)[0];
-    if(filename === fileBeingPlayed) {
-      $cpuSquares.eq(currentIndex).stop(true, false);
-      if (clickCounter%2 === 0) {
+      var parent = $("#userSquares");
+      var filename = $(this).data('filename') + ".wav";
+      var fileBeingPlayed = audio.src.split('/').slice(-1)[0];
+      if(filename === fileBeingPlayed) {
+        $cpuSquares.eq(currentIndex).stop(true, false);
+        if (clickCounter%2 === 0) {
 
+          $playerTurn.text('Player 1 to Play');
 
-        user1Score++;
-        console.log(user1Score);
-        $score1.text(user1Score);
-        $(this).css({background: "red"});}
-      else {
-        user2Score++;
-        console.log(user2Score);
-        $score2.text(user2Score);
-        $(this).css({background: "green"});
+          user1Score++;
+          console.log(user1Score);
+          $score1.text(user1Score);
+          $(this).css({background: "red"});
+        } else {
+          $playerTurn.text('Player 2 to Play');
+
+          user2Score++;
+          console.log(user2Score);
+          $score2.text(user2Score);
+          $(this).css({background: "green"});
+        }
+      } else {
+        $(this).css({ opacity: 0 });
+        if (clickCounter%2 === 0) {
+          $playerTurn.text('Player 1 to Play');
+        } else {
+          $playerTurn.text('Player 2 to Play');
+        }
+      }
+
+    });
+
+    function getWinner (a, b) {
+      console.log(a, b);
+      if (a > b) {
+        $result.text("Player 1 Wins");
+        console.log("player1");
+      } else if (b > a)  {
+        $result.text("Player 2 Wins");
+        console.log("Player2");
+      } else {
+        $result.text("It's a Draw");
+        console.log("draw");
       }
     }
-    else {
-      $(this).css({ opacity: 0 });
-    }
-    var theResult = getWinner(user1Score, user2Score);
-    console.log("result " + theResult);
-  });
-
-  function getWinner (a, b) {
-  console.log(a, b);
-  if (a > b ) {
-      document.getElementById("result").textContent = "Player 1 Wins this time";
-      console.log("player1");
-  } else if (b > a )  {
-      document.getElementById("result").textContent = "Player 2 Wins this time";
-      console.log("Player2");
-  } else {
-      document.getElementById("result").textContent = "It's a Draw";
-      console.log("draw");
-  }
-  }
 
   }
 
-
+//}
 });
