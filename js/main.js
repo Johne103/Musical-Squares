1504846d2a88ebb1789ddf8ad9a70e62e58dbaca
John Evans <johne103@hotmail.com>
2016-10-12 08:23:40 +0100

    Updated main.html & css

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 //WDI-idn-23 integer project 1. created by John Evans release 1.0 12 October 2016.
 //First project to create a game in order to develop an improved understanding of the approach required to create and deploy for general use a viable working web based game.
 
-//Musical Squares is a 2 player game that tests the visual and auditory response times of users. A random grid of images and sounds are presented to the players with the aim of capturing a square that is the process of being removed from the grid.
+//Musical Squares is a 1 or 2 player game that tests the visual and auditory response times of users.
+
+//There is potential for application within the medical arena to measure an individuals capacity to responde to visual and auditory stimuation and abliity to concentrate over a short time period.
+
+//A random grid of images and sounds are presented to the players with the aim of capturing a square that is the process of being removed from the grid.
 
 //The game has 3 levels of easy, medium and hard with corresponding reductions in reaction times at 5 seconds, 3 seconds and 1 second.
 
-//The player with the most captured images of the 8 possible turns each; wins.
+//The player with the most captured images of the 8 possible turns each; wins. As a 1 player game the user would play against the board with the aim of capturing as many squares as possible.
 
 //Instructions for playing the game: Click on either the EASY, MEDIUM or HARD buttons to start the game. Click RESET to restart.
 
 //Take turns to click on the image on the right hand grid before the corresponding image and sound. disapears on the left hand grid. Score points when you correctly click the correct image on the right before the image on the left disapears completely.
 
 //If you click on the wrong image it will be removed from the grid and you will lose your turn.
+
+//Known bugs: Computer may at times freeze part way through the game.
+//On reset the computer may record and additional click when only one click was intended.
 
 
 function shuffle(a) {                                         //Randomise the image and the sounds for the left grid.
